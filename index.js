8a996f5844a365307a434ee6d070ec20fe54d75e
julianxlozano <julianxlozano@gmail.com>
2021-06-02 17:17:11 -0500

    commit tree 01b879f764fc826d5d99de2fddfae9976fdfa6f6

--- index.js
+++ index.js
@@ -13,10 +13,71 @@ let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
-
     let payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
     return payable
 }
+
+
+function createEmployeeRecord(employeeDeetsArray){
+    const employeeDeetsObject = {
+        'firstName':employeeDeetsArray[0],
+        'familyName':employeeDeetsArray[1],
+        'title':employeeDeetsArray[2],
+        'payPerHour':employeeDeetsArray[3],
+        'timeInEvents':[],
+        'timeOutEvents':[]
+    };
+    return employeeDeetsObject
+};
+
+
+function createEmployeeRecords(employeeDeetsArrays){
+    let newEmployeeDeetsArray = [];
+    employeeDeetsArrays.forEach(employeeDeetsArray=>{newEmployeeDeetsArray.push(createEmployeeRecord(employeeDeetsArray))})
+    return newEmployeeDeetsArray
+};
+
+function createTimeInEvent(dateStamp){
+    this['timeInEvents'].push({'type': 'TimeIn',
+                                         'hour': parseInt(dateStamp.split(" ")[1]),
+                                         'date': dateStamp.split(" ")[0]
+                                        })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    this['timeOutEvents'].push({'type': 'TimeOut',
+                                         'hour': parseInt(dateStamp.split(" ")[1]),
+                                         'date': dateStamp.split(" ")[0]
+                                        })
+    return this
+}
+
+function hoursWorkedOnDate(formDate){
+    let TimeOut = this['timeOutEvents'].filter(e =>{return e.date === formDate})
+    let TimeIn = this['timeInEvents'].filter(e =>{return e.date === formDate})
+         let result = TimeOut[0]['hour'] - TimeIn[0]['hour']
+         let result2 = result.toString()
+        return parseInt(result2.substring(0,result2.length -2))   
+ }
+ 
+ function wagesEarnedOnDate(formDate){
+         return parseInt(hoursWorkedOnDate.call(this,formDate) * this['payPerHour'])   
+  }
+
+  function calculatePayroll(empRecArray){
+    let allWages = empRecArray.map(empRec => {let allWagesInside = allWagesFor.bind(empRec)
+                                             return allWagesInside(empRec)})
+    let totalWagesEarned = allWages.reduce(function(total,element){return element + total})
+    return totalWagesEarned
+ }
+ 
+
+
+
+ function findEmployeeByFirstName(srcArray,firstName){
+    let results = srcArray.filter(employeeRecord =>{return employeeRecord['firstName'] === firstName})
+    return results[0]
+  }
